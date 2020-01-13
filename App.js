b96ddf31218e717d990ecd1ec0f7124132874885
Logan Angellotti <loganangellotti@aliasappss-iMac.local>
2020-01-13 12:16:17 -0500

    work on sub switching

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, Image, StyleSheet, TextInput, Button, TouchableNativeFeedback, Platform, TouchableHighlight, TouchableOpacity, TouchableWithoutFeedback, ScrollView, FlatList, SectionList, DrawerLayoutAndroid, PanResponder, Animated } from 'react-native';
+import { Text, View, Image, StyleSheet, TextInput, Button, TouchableNativeFeedback, Platform, TouchableHighlight, TouchableOpacity, TouchableWithoutFeedback, ScrollView, FlatList, SectionList, DrawerLayoutAndroid, PanResponder, Animated, Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native';
 import { removeOrientationChangeListener } from 'expo/build/ScreenOrientation/ScreenOrientation';
 
 export default class FieldCypher extends Component {
@@ -7,29 +7,62 @@ export default class FieldCypher extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { text: '', cypher: 'Substitution', sub: 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z' };
+        this.state = {
+            text: '',
+            cypher: 'Substitution',
+            subst: 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z',
+        };
+        this.letters = 'abcdefghijklmnopqrstuvwxyz'
         this.switchToPoly = this.switchToPoly.bind(this)
         this.switchToSub = this.switchToSub.bind(this)
         this.switchSubToAtbash = this.switchSubToAtbash.bind(this)
         this.resetSub = this.resetSub.bind(this)
+        this.changesub = this.changesub.bind(this)
+        this.test = this.test.bind(this)
     }
 
     render() {
+
+        //class Editbox extends Component {
+        //    render() {
+        //        return (
+        //            <TextInput
+        //                style={{ height: 20, textAlign: 'center' }}
+        //                placeholder={this.props.letter}
+        //                onChangeText={(text) => this.changesub(this.props.letter, text)}
+        //            />
+        //        )
+        //    }
+        //}
+
+        let Editbox =
+            < TextInput
+                style={{ height: 20, textAlign: 'center' }
+                }
+                placeholder={this.props.letter}
+                onChangeText={(text) => this.changesub(this.props.letter, text)}
+            />
+
+
+
         let button;
         if (this.state.cypher == 'Substitution') {
-            button = 
-            <View>
-            <Button
-            onPress={this.switchSubToAtbash}
-            title="Atbash Template"
-            />
-            <Button
-            onPress={this.resetSub}
-            title="Reset substitution template"
-            />
-            </View>
+            button =
+                <View>
+                    <Text style={{ textAlign: 'center' }}>
+                        {this.state.test}
+                    </Text>
+                    <Button
+                        onPress={this.switchSubToAtbash}
+                        title="Atbash Template"
+                    />
+                    <Button
+                        onPress={this.resetSub}
+                        title="Reset substitution template"
+                    />
+                </View>
         } else {
-            button = <Text/>
+            button = <Text />
         }
         return (
 
@@ -67,11 +100,32 @@ export default class FieldCypher extends Component {
                             onPress={this.switchToSub}
                             title="Substitution"
                         />
+                        <Button
+                            onPress={this.test}
+                            title="test"
+                        />
+                        <View letter='a'>
+                        {Editbox}
+                        </View>
                     </View>
                 </View>
             </View>
 
         );
+    }
+
+    test = function () {
+        this.changesub('a', 'b')
+    }
+
+    changesub = function (letter, change) {
+        var changing = this.state.subst.split(',')
+        var index = this.letters.indexOf(letter)
+        changing[index] = change
+
+        this.setState({
+            subst: changing.toString()
+        })
     }
 
     switchToPoly = function () {
@@ -87,23 +141,22 @@ export default class FieldCypher extends Component {
 
     switchSubToAtbash = function () {
         this.setState({
-            sub: 'z,y,x,w,v,u,t,s,r,q,p,o,n,m,l,k,j,i,h,g,f,e,d,c,b,a'
+            subst: 'z,y,x,w,v,u,t,s,r,q,p,o,n,m,l,k,j,i,h,g,f,e,d,c,b,a'
         })
     }
 
     resetSub = function () {
         this.setState({
-            sub: 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'
+            subst: 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'
         })
     }
 
     SUB = function (toCode) {
-        var sub = this.state.sub.split(',')
-        var letters = 'abcdefghijklmnopqrstuvwxyz'
+        var subst = this.state.subst.split(',')
         var tc = toCode.toLowerCase().split('')
         for (var i = 0; i < tc.length; i++) {
-            if (letters.indexOf(tc[i]) > -1) {
-                tc[i] = sub[letters.indexOf(tc[i])]
+            if (this.letters.indexOf(tc[i]) > -1) {
+                tc[i] = subst[this.letters.indexOf(tc[i])]
             }
         }
 
@@ -112,6 +165,19 @@ export default class FieldCypher extends Component {
 }
 
 
+const styles = StyleSheet.create({
+    bigblue: {
+        textAlignVertical: 'center',
+        textAlign: 'center',
+        height: 30,
+        width: 30,
+        color: 'blue',
+        fontWeight: 'bold',
+        fontSize: 20,
+        borderColor: 'black',
+        borderWidth: 1
+    }
+});
 
 
 /*
